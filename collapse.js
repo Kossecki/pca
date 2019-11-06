696f5defd0262cb66fd8839a5abc3187fef0f7e8
Martin Uzak <martin.uzak@gmail.com>
2019-11-06 10:52:45 +0100

    remove obsolete code

--- collapse.js
+++ collapse.js
@@ -1,29 +1,14 @@
+// mobile 'menu' button
 let element = document.getElementById('mobile');
-let list = document.getElementById('list');
-
 
 element.onclick = function () {
     list.classList.toggle('visible');
 }
 
-let items = document.getElementsByClassName("menu-item");
-for (var i = 0; i < items.length; i++) {
-    items[i].onclick = function () {
-        list.classList.remove('visible');
-    }
-}
-
-
-
+// desktop language switcher
 let langsOptions = document.getElementById("langs-switcher_options");
 let active = document.getElementById("active");
 
 active.onclick = () => {
-    langsOptions.classList.add('visible');
-let langs = document.getElementsByClassName("navbar-list-item-lang");
-for (var i = 0; i < langs.length; i++) {
-    langs[i].onclick = function () {
-        langsOptions.classList.remove('visible');
-    }
-}
+    langsOptions.classList.toggle('visible');
 }
