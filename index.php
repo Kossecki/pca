3e612ce5b6581541d76a0707cbd7da80d4ecc59d
Martin Uzak <martin.uzak@gmail.com>
2019-11-13 16:45:00 +0100

    add redirects based upon client's language

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$supported_langs = array("en", "cz", "sk", "de");
+$default_lang = "cz";
+
+$accept_lang = $_SERVER["HTTP_ACCEPT_LANGUAGE"];
+$lang = substr($accept_lang, 0, 2);
+
+if (!in_array($lang , $supported_langs)) {
+    $lang = $default_lang;
+} 
+
+header("location: index-$lang.html");
+?>
